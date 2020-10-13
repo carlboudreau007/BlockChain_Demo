58c2c512da6b693c66b06f94c966a093fb3c52cc
Boudreau, Carl <carl_boudreau@optum.com>
2020-10-13 10:42:49 -0500

    updating the local_app and app to be trigered either from the local host and
    Updating the local_app and app to be trigered either from the local host and  Azure

---
+++ local_app.py
@@ -0,0 +1,11 @@
+from waitress import serve
+import socket
+
+from app import app
+
+if __name__ == '__main__':
+    DASHBOARD_PORT = 5000
+    IP_ADDRESS = socket.gethostbyname(socket.gethostname())
+
+    serve(app, host=IP_ADDRESS, port=DASHBOARD_PORT, threads=20)
+    
